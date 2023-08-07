217fde009621336734ded45f41467b6bbea53670
Evance Odoyo <odoyoevanc@gmail.com>
2023-08-07 22:47:01 +0300

    remove unused imports and add changes for reviews

--- web_dynamic/100-hbnb.py
+++ web_dynamic/100-hbnb.py
@@ -2,10 +2,8 @@
 """ Starts a Flash Web Application """
 from models import storage
 from models.state import State
-from models.city import City
 from models.amenity import Amenity
 from models.place import Place
-from os import environ
 from flask import Flask, render_template
 from uuid import uuid4
 app = Flask(__name__)
