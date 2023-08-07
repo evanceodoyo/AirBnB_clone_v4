9b7e5ab090710f66510eb9743f6a27e1f8c47cc8
Evance Odoyo <odoyoevanc@gmail.com>
2023-08-08 02:23:13 +0300

    update 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -17,9 +17,9 @@ function initialize () {
 function apiStatus () {
   $.get('http://0.0.0.0:5001/api/v1/status/', (data, textStatus) => {
     if (textStatus === 'success' && data.status === 'OK') {
-      $('#api_status').addClass('available');
+      $('div#api_status').addClass('available');
     } else {
-      $('#api_status').removeClass('available');
+      $('div#api_status').removeClass('available');
     }
   });
 }
