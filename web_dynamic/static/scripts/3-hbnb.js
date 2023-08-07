1c7e9d9a3740f08e94fa734473fdcc44a175d4ef
Evance Odoyo <odoyoevanc@gmail.com>
2023-08-08 02:51:15 +0300

    fetch places

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,14 +1,13 @@
 $(document).ready(initialize);
-
 function initialize () {
   const amenity = {};
-  $('.amenities .popover input').change(function () {
+  $('.amenities .popover input').on('change', function () {
     if ($(this).is(':checked')) {
-      amenity[$(this).attr('data-name')] = $(this).attr('data-id');
-    } else if ($(this).is(':not(:checked)')) {
-      delete amenity[$(this).attr('data-name')];
+      amenity[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenity[$(this).attr('data-id')];
     }
-    const amenities = Object.keys(amenity);
+    const amenities = Object.values(amenity);
     $('.amenities h4').text(amenities.sort().join(', '));
   });
   apiStatus();
@@ -18,9 +17,9 @@ function initialize () {
 function apiStatus () {
   $.get('http://0.0.0.0:5001/api/v1/status/', (data, textStatus) => {
     if (textStatus === 'success' && data.status === 'OK') {
-      $('#api_status').addClass('available');
+      $('div#api_status').addClass('available');
     } else {
-      $('#api_status').removeClass('available');
+      $('div#api_status').removeClass('available');
     }
   });
 }
