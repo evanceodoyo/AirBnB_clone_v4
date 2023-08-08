d8b685e6313f66ea7bec61a007cadd5380dfbec3
Evance Odoyo <odoyoevanc@gmail.com>
2023-08-08 04:06:05 +0300

    properly filter according to amenities

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,37 +1,42 @@
 $(document).ready(initialize);
-
 function initialize () {
   const amenity = {};
-  $('.amenities .popover input').change(function () {
+  const objIds = {};
+
+  $('.amenities .popover input').on('change', function () {
     if ($(this).is(':checked')) {
-      amenity[$(this).attr('data-name')] = $(this).attr('data-id');
-    } else if ($(this).is(':not(:checked)')) {
-      delete amenity[$(this).attr('data-name')];
+      amenity[$(this).attr('data-id')] = $(this).attr('data-name');
+    } else {
+      delete amenity[$(this).attr('data-id')];
     }
-    const amenities = Object.keys(amenity);
+    const amenities = Object.values(amenity);
+    objIds.amenities = Object.keys(amenity);
     $('.amenities h4').text(amenities.sort().join(', '));
   });
   apiStatus();
-  getPlaceAmenity();
+  handleButtonClick(objIds);
+  getPlaces({});
 }
 
 function apiStatus () {
   $.get('http://0.0.0.0:5001/api/v1/status/', (data, textStatus) => {
     if (textStatus === 'success' && data.status === 'OK') {
-      $('#api_status').addClass('available');
+      $('div#api_status').addClass('available');
     } else {
-      $('#api_status').removeClass('available');
+      $('div#api_status').removeClass('available');
     }
   });
 }
 
-function getPlaceAmenity () {
+function getPlaces (objIds) {
   $.ajax({
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     type: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    data: JSON.stringify({ amenities: Object.values(amenity) }),
+    data: JSON.stringify(objIds),
     success: function (response) {
+      $('SECTION.places').empty();
+
       for (const content of response) {
         const data = [
           '<article>',
@@ -55,5 +60,11 @@ function getPlaceAmenity () {
     error: function (error) {
       console.log(error);
     }
+  });
+}
+
+function handleButtonClick (objIds) {
+  $('button').on('click', function () {
+    getPlaces(objIds);
   });
 }
